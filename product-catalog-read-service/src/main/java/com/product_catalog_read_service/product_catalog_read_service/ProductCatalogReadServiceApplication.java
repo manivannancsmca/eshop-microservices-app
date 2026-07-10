6bac597873529c1fae70b95078889336c6c1ed3f
manivannancsmca <manivannancsmca@gmail.com>
2026-07-11 00:20:42 +0530

    product read application created

---
+++ product-catalog-read-service/src/main/java/com/product_catalog_read_service/product_catalog_read_service/ProductCatalogReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.product_catalog_read_service.product_catalog_read_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogReadServiceApplication.class, args);
+	}
+
+}
---
+++ product-catalog-read-service/src/test/java/com/product_catalog_read_service/product_catalog_read_service/ProductCatalogReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product_catalog_read_service.product_catalog_read_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
